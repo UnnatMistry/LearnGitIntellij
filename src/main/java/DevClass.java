2f22c05837eb009784a45f2077673eb234ac262d
VasuMPatel <87130927+VasuMPatel@users.noreply.github.com>
2023-04-17 04:04:39 -0400

    Update DevClass.java

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,6 +1,7 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("DevClass");
+        System.out.println("DevClass + Stream2Learn");
         // This is a commentsds
+        // Checkiin from Stream2 learn
     }
 }
