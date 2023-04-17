d6ee63101ad6eb3c1006d3af4d51f1be87f95969
unnat.mistry <unnatmistry@icloud.com>
2023-04-17 08:51:13 -0400

    Added a print message

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -2,8 +2,6 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("DevClass+Class");
         System.out.println("Zebra");
-        System.out.println("a");
-        System.out.println("jj");
     System.out.println("b");
     }
 }
