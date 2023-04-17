c64e7a71d3f8e5a710a6d9ae76154ee4ff457b42
unnat.mistry <unnatmistry@icloud.com>
2023-04-17 08:48:59 -0400

    Added a print message

--- src/main/java/SecondClass.java
+++
@@ -1,7 +0,0 @@
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
-public class SecondClass {
-    public static void main(String[] args) {
-        System.out.println("Added a print statement");
-    }
-}
