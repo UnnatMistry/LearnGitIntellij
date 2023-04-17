62838688cddf273bd04f32f006863877365e5385
VasuMPatel <87130927+VasuMPatel@users.noreply.github.com>
2023-04-17 04:34:21 -0400

    Update DevClass.java

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,6 +1,7 @@
 public class DevClass {
     public static void main(String[] args) {
         System.out.println("DevClass");
+        System.out.println("this is second line");
         // This is a commentysds
     }
 }
